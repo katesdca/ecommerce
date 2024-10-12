2adbb1cc91ef0868b497106ab9a8dfa479175b83
Kateeeee <kate.palapuz@sdca.edu.ph>
2024-10-12 19:05:36 +0800

    implemented regis

---
+++ authRegister.php
@@ -0,0 +1,60 @@
+<?php
+    $fullName = $_POST["fullName"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        //RECEIVED USER INPUT
+
+        //VERIFY PASSWORD AND CONFIRMPASS TO BE MATCH
+        if(trim($password) == trim($confirmPassword))
+        {
+            
+         //CONNECT DATABASE
+        
+        
+         
+            $host = "localhost";
+            $database = "ecomerceb2";
+            $dbuserame = "root";
+            $dbpassword = "";
+            
+            $dsn = "mysql: host=$host;dbname=$database;";
+            try {
+                $conn = new PDO($dsn, $dbuserame, $dbpassword);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //p_fullName,$fullName
+                $stmt = $conn->prepare("INSERT INTO users (fullName,username,password,created_at,updated_at) VALUES (:p_fullName,:p_username,:p_password,NOW(),NOW())");
+                $stmt->bindParam(':p_fullName', $fullName);
+                $stmt->bindParam(':p_username', $username);
+                $stmt->bindParam(':p_password', $password);
+
+
+                $password = password_hash(trim($password),PASSWORD_BCRYPT);
+               
+                if($stmt->execute()){
+                    header("location: /registration.php?success=Registration successful");
+                    exit;
+                  
+                }else{
+                    header("location: /registration.php?error=Insert Error");
+                    exit;
+                }
+
+            } catch (Exception $e){
+                header("location: /registration.php?error=Username Already Existing");
+            }
+        }
+            else
+            {
+                header("location: /registration.php?error=Password Mismatch");
+                exit;
+            }
+                  
+       
+        //INSERT RECORD
+        }
+    ?>
+    
